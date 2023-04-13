1a92eca3123dc68c645cdae9258f6bad2eb73cc8
ybadr99 <yousefbadr909@gmail.com>
2023-04-13 08:39:14 +0200

    fix linters

--- js/main.js
+++ js/main.js
@@ -1,134 +1,123 @@
 // -- projects section
 const projects = [
   {
-    id:1,
-    name:'Multi-Post Stories Gain+Glory',
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-    featuredImage:"./images/projects/project03.png",
-    technologies:['Ruby on Rails','css','JavaScript',"HTML"] ,
-    liveLink:"https://ybadr99.github.io/portfolio/",
-    sourceLink:"https://github.com/ybadr99/portfolio"
+    id: 1,
+    name: 'Multi-Post Stories Gain+Glory',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+    featuredImage: './images/projects/project03.png',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'HTML'],
+    liveLink: 'https://ybadr99.github.io/portfolio/',
+    sourceLink: 'https://github.com/ybadr99/portfolio',
   },
   {
-    id:2,
-    name:'Multi-Post Stories Gain+Glory',
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-    featuredImage:"./images/projects/project02.png",
-    technologies:['Ruby on Rails','css','JavaScript',"HTML"] ,
-    liveLink:"https://ybadr99.github.io/portfolio/",
-    sourceLink:"https://github.com/ybadr99/portfolio"
+    id: 2,
+    name: 'Multi-Post Stories Gain+Glory',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+    featuredImage: './images/projects/project02.png',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'HTML'],
+    liveLink: 'https://ybadr99.github.io/portfolio/',
+    sourceLink: 'https://github.com/ybadr99/portfolio',
   },
   {
-    id:3,
-    name:'Multi-Post Stories Gain+Glory',
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-    featuredImage:"./images/projects/project03.png",
-    technologies:['Ruby on Rails','css','JavaScript',"HTML"] ,
-    liveLink:"https://ybadr99.github.io/portfolio/",
-    sourceLink:"https://github.com/ybadr99/portfolio"
+    id: 3,
+    name: 'Multi-Post Stories Gain+Glory',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+    featuredImage: './images/projects/project03.png',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'HTML'],
+    liveLink: 'https://ybadr99.github.io/portfolio/',
+    sourceLink: 'https://github.com/ybadr99/portfolio',
   },
   {
-    id:4,
-    name:'Multi-Post Stories Gain+Glory',
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-    featuredImage:"./images/projects/project02.png",
-    technologies:['Ruby on Rails','css','JavaScript',"HTML"] ,
-    liveLink:"https://ybadr99.github.io/portfolio/",
-    sourceLink:"https://github.com/ybadr99/portfolio"
+    id: 4,
+    name: 'Multi-Post Stories Gain+Glory',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+    featuredImage: './images/projects/project02.png',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'HTML'],
+    liveLink: 'https://ybadr99.github.io/portfolio/',
+    sourceLink: 'https://github.com/ybadr99/portfolio',
   },
   {
-    id:5,
-    name:'Multi-Post Stories Gain+Glory',
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-    featuredImage:"./images/projects/project03.png",
-    technologies:['Ruby on Rails','css','JavaScript',"HTML"] ,
-    liveLink:"https://ybadr99.github.io/portfolio/",
-    sourceLink:"https://github.com/ybadr99/portfolio"
+    id: 5,
+    name: 'Multi-Post Stories Gain+Glory',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+    featuredImage: './images/projects/project03.png',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'HTML'],
+    liveLink: 'https://ybadr99.github.io/portfolio/',
+    sourceLink: 'https://github.com/ybadr99/portfolio',
   },
   {
-    id:6,
-    name:'Multi-Post Stories Gain+Glory',
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-    featuredImage:"./images/projects/project02.png",
-    technologies:['Ruby on Rails','css','JavaScript',"HTML"] ,
-    liveLink:"https://ybadr99.github.io/portfolio/",
-    sourceLink:"https://github.com/ybadr99/portfolio"
+    id: 6,
+    name: 'Multi-Post Stories Gain+Glory',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+    featuredImage: './images/projects/project02.png',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'HTML'],
+    liveLink: 'https://ybadr99.github.io/portfolio/',
+    sourceLink: 'https://github.com/ybadr99/portfolio',
   },
 
-]
+];
 
+const menuIcon = document.querySelector('.menu-icon');
+const mobileMenu = document.querySelector('.mobile-nav-items');
+const navItem = document.querySelectorAll('.nav-item');
 
-
-const menuIcon = document.querySelector(".menu-icon");
-const mobileMenu = document.querySelector(".mobile-nav-items");
-const navItem = document.querySelectorAll(".nav-item");
-
-
-const projectsContainer = document.getElementById('projects')
+const projectsContainer = document.getElementById('projects');
 // popup window
-const modal = document.querySelector("#popup-window");
-const close = document.querySelector(".close");
-const modalContent = document.querySelector('.modal-content')
+const modal = document.querySelector('#popup-window');
+const close = document.querySelector('.close');
+const modalContent = document.querySelector('.modal-content');
 
-
-
-
-
-menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("active");
-  mobileMenu.classList.toggle("active");
-  document.body.classList.toggle("hidden-flow");
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+  document.body.classList.toggle('hidden-flow');
 });
 
 navItem.forEach((n) => {
-  n.addEventListener("click", () => {
-    menuIcon.classList.remove("active");
-    mobileMenu.classList.remove("active");
-    document.body.classList.remove("hidden-flow");
+  n.addEventListener('click', () => {
+    menuIcon.classList.remove('active');
+    mobileMenu.classList.remove('active');
+    document.body.classList.remove('hidden-flow');
   });
 });
 
-
-
 const createProject = (project) => {
-  const projectEl = document.createElement('div')
-  projectEl.classList.add('project')
+  const projectEl = document.createElement('div');
+  projectEl.classList.add('project');
 
-  const img = document.createElement('img')
-  img.src = project.featuredImage
+  const img = document.createElement('img');
+  img.src = project.featuredImage;
 
-  const name = document.createElement('h3')
-  name.classList.add('project-heading')
-  name.innerText = project.name
+  const name = document.createElement('h3');
+  name.classList.add('project-heading');
+  name.innerText = project.name;
 
-  const ul = document.createElement('ul')
-  ul.classList.add('techs')
+  const ul = document.createElement('ul');
+  ul.classList.add('techs');
 
-  project.technologies.forEach(el=>{
-    const li = document.createElement('li')
-    li.classList.add('techs-item')
+  project.technologies.forEach((el) => {
+    const li = document.createElement('li');
+    li.classList.add('techs-item');
 
-    li.innerText = el
-    ul.appendChild(li)
-  })
+    li.innerText = el;
+    ul.appendChild(li);
+  });
 
-  const btn = document.createElement('button')
-  btn.classList = 'btn see-project'
-  btn.id = project.id
-  btn.innerText = "See project"
+  const btn = document.createElement('button');
+  btn.classList = 'btn see-project';
+  btn.id = project.id;
+  btn.innerText = 'See project';
 
-  projectEl.appendChild(img)
-  projectEl.appendChild(name)
-  projectEl.appendChild(ul)
-  projectEl.appendChild(btn)
+  projectEl.appendChild(img);
+  projectEl.appendChild(name);
+  projectEl.appendChild(ul);
+  projectEl.appendChild(btn);
 
-  return projectEl
-}
+  return projectEl;
+};
 
 // const modalContainer = document.querySelector('.modal-container')
-const renderPopup = (project) =>{
-
-  return `
+const renderPopup = (project) => `
 
   <div class="img-box">
     <img
@@ -165,40 +154,31 @@ const renderPopup = (project) =>{
     </li>
   </ul>
 
-`
-}
+`;
 
-
-
-window.onload = () =>{
-
-  projects.forEach(project=>{
-    projectsContainer.appendChild(createProject(project))
-  })  
-
+window.onload = () => {
+  projects.forEach((project) => {
+    projectsContainer.appendChild(createProject(project));
+  });
 
   const projectBtns = document.querySelectorAll('.see-project');
   projectBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
       const project = projects.find((project) => project.id === +btn.id);
-      console.log(project)
-      modalContent.innerHTML = renderPopup(project)
+      modalContent.innerHTML = renderPopup(project);
       modal.style.display = 'block';
     });
   });
-}
+};
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  console.log(event)
-  close.onclick = ()=> {
-    modal.style.display = "none";
+document.addEventListener('DOMContentLoaded', (event) => {
+  close.onclick = () => {
+    modal.style.display = 'none';
   };
-  
-  window.onclick =  (e)=> {
-    if (e.target == modal) {
-      modal.style.display = "none";
+
+  window.onclick = (e) => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
     }
   };
-
 });
-
